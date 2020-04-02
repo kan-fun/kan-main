b69bc335000d26a8f6a01cbe9456235a39039293
Your Name <you@example.com>
2020-04-02 09:57:30 +0800

    Init deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+wget https://webplus-cn-shenzhen.oss-cn-shenzhen.aliyuncs.com/cli/wpctl-linux
+mv ./wpctl-linux ./wpctl
+chmod +x ./wpctl
+
+./wpctl configure --access-key-id "$ALICLOUD_ACCESS_KEY" --access-key-secret "$ALICLOUD_SECRET_KEY" --region "$ALICLOUD_REGION"
+
+go build .
+zip upload.zip kan-main
+
+./wpctl env:deploy upload.zip --app Kan --env KanEnv
