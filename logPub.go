9c4e709cced2954f56cce0edf17cc7c516f6ca00
Your Name <you@example.com>
2020-04-05 21:17:36 +0800

    Add checkSignature in logPub

--- logPub.go
+++ logPub.go
@@ -8,8 +8,16 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func logPub(c *gin.Context) {
+	// Check Signature and Get User(Contain id, secret_key)
+	_, err := checkSignature(c, nil)
+	if err != nil {
+		c.String(403, err.Error())
+		return
+	}
+
 	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
+		c.String(403, err.Error())
 		return
 	}
 	defer ws.Close()
